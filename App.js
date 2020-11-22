b12bfcedd7da1af67209344616d94e38452a20e5
meghabalaraju <kbmegha4@outlook.com>
2020-11-22 19:22:44 +0100

    Initial project setup

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
 
-export default function App() {
+function MFStatusBar({ backgroundColor, ...props }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <MFStatusBar backgroundColor="#8db596" barStyle="light-content" />
+      <Text>Env for mobile flashcards</Text>
+    </View>
+  );
+}
