5cf572eddf3119a02914820e1f4db663379df7d0
meghabalaraju <kbmegha4@outlook.com>
2020-12-15 23:48:39 +0100

    minor changes, fixed component name error and remove unused pros

--- utils/helpers.js
+++ utils/helpers.js
@@ -19,7 +19,6 @@ async function setDummyData() {
     "6ni6ok3ym7mf1p33lnez": {
       id: "6ni6ok3ym7mf1p33lnez",
       title: "React Native deck",
-      totalCards: 3,
       cards: [
         {
           questionText: "Does React Native work with Android?",
@@ -38,7 +37,6 @@ async function setDummyData() {
     "8xf0y6ziyjabvozdd253nd": {
       id: "8xf0y6ziyjabvozdd253nd",
       title: "React deck",
-      totalCards: 0,
       cards: [],
     },
   };
