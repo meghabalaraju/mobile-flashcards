61e057a7ccfd962493774a6a72920d3f2f853cec
meghabalaraju <kbmegha4@outlook.com>
2020-12-15 23:14:32 +0100

    Feat: set notification implemented

--- components/Quiz.js
+++ components/Quiz.js
@@ -8,6 +8,10 @@ import TextButton from "./UIComponents/TextButton";
 
 // imports utils
 import { tabText, white } from "../utils/colors";
+import {
+  clearNotifications,
+  setLocalNotifications,
+} from "../utils/notification";
 
 class Quiz extends Component {
   state = {
@@ -32,7 +36,7 @@ class Quiz extends Component {
   // change question after each question is answred by user
   changeQuestion = () => {
     const { cards } = this.props;
-    const { index } = this.state;
+    const { index, isComplete } = this.state;
     const newCard = cards[index];
 
     // if there are no more questions left to show, update the state
@@ -41,6 +45,10 @@ class Quiz extends Component {
         this.setState(() => ({
           isComplete: true,
         }));
+      }
+
+      if (isComplete) {
+        clearNotifications().then(setLocalNotifications);
       }
     }
     return newCard;
@@ -52,6 +60,19 @@ class Quiz extends Component {
     this.setState(() => ({
       card: newQuestion,
       index: index + 1,
+    }));
+  };
+
+  // reset state to initial state and go back to deck details screen
+  resetSate = () => {
+    // redirect to deck details
+    this.props.navigation.goBack();
+
+    // reset to initial state
+    this.setState(() => ({
+      card: {},
+      index: 0,
+      score: 0,
     }));
   };
 
@@ -91,7 +112,7 @@ class Quiz extends Component {
           <Text style={styles.score}>You scored: {scoreInPercent}%</Text>
           <TextButton
             style={([styles.textBtn], { letterSpacing: 1, fontSize: 20 })}
-            onPress={() => navigation.goBack()}
+            onPress={this.resetSate}
           >
             Back to Deck
           </TextButton>
